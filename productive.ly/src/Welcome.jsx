87626f582a09b903de77404e33dcee0eab9c9536
Andrew <aawferris@gmail.com>
2020-10-09 10:41:21 -0400

    move libraries for now, render welcome

---
+++ productive.ly/src/Welcome.jsx
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+
+export default class Welcome extends Component {
+  constructor() {
+    super()
+  }
+
+  render() {
+    return (
+      <div>
+        <header>
+          <h1>Welcome to Productive.ly</h1>
+        </header>
+      </div>
+    )
+  }
+  
+};
