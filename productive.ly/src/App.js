a224075cf477b25f0b4988b4f812ecafb28d549e
Andrew <aawferris@gmail.com>
2020-10-13 16:45:49 -0400

    random background fully working

--- productive.ly/src/App.js
+++ productive.ly/src/App.js
@@ -1,9 +1,8 @@
-import React, { Component } from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 import "./Style Library/App.css";
 
 import Advice from "./Component_Library/Advice";
-// import BackGroundGet from "./Component_Library/BackGroundGet";
-import BackGround from "./Component_Library/BackGround";
 import BreakButton from "./Component_Library/BreakButton";
 import Countdown from "./Component_Library/Countdown";
 import NotesGet from "./Component_Library/NotesGet";
@@ -13,9 +12,28 @@ import WelcomePost from "./Component_Library/WelcomePost";
 import WelcomeGet from "./Component_Library/WelcomeGet";
 
 function App() {
+  const [image, setImage] = useState("");
+
+  useEffect(() => {
+    const getApi = async () => {
+      const url = `https://pixabay.com/api/?key=${process.env.REACT_APP_PIXABAY_KEY}&q=nature&image_type=photo&orientation=horizontal`;
+      const response = await axios.get(url);
+      console.log(response.data.hits);
+      const imageArr = response.data.hits;
+      const largeImage =
+        imageArr[Math.floor(Math.random() * imageArr.length)].largeImageURL;
+      console.log(largeImage);
+      setImage(largeImage);
+    };
+    getApi();
+  }, []);
+
+  let backgroundStyle = {
+    backgroundImage: `url(${image})`,
+  };
+
   return (
     <div className="main-container" style={backgroundStyle}>
-      <BackGround />
       <WelcomeGet />
       <WelcomePost />
       <Countdown />
