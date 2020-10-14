5a862872515ef7f78b949889d8e82491d51aee0b
Andrew <aawferris@gmail.com>
2020-10-14 11:05:23 -0400

    switch styling over to grid

--- productive.ly/src/App.js
+++ productive.ly/src/App.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
-import "./Style Library/App.css";
+import "./Style Library/AppGrid.css";
 
 import Advice from "./Component_Library/Advice";
 import BreakButton from "./Component_Library/BreakButton";
@@ -56,15 +56,36 @@ function App() {
   }, []);
 
   return (
-    <div className="main-container" style={backgroundStyle}>
-      <Welcome name={name} />
-      <WelcomePost />
-      <Countdown />
-      <Tasks />
-      <NotesGet />
-      <NotesPost />
-      <BreakButton />
-      <Advice />
+    <div className="app">
+      <div className="layout" style={backgroundStyle}>
+        <header>Productive.ly</header>
+        <nav>Nav</nav>
+        <article>
+          <div className="main-welcome">
+            <Welcome name={name} />
+            <WelcomePost />
+          </div>
+        </article>
+        <section>
+          <div className="main-countdown">
+            <Countdown />
+          </div>
+          <div className="main-break">
+            <BreakButton />
+          </div>
+          <div className="main-advice">
+            <Advice />
+          </div>
+        </section>
+        <div id="notes">
+          <NotesGet />
+          <NotesPost />
+        </div>
+        <div id="tasks">
+          <Tasks />
+        </div>
+        <footer>Footer</footer>
+      </div>
     </div>
   );
 }
