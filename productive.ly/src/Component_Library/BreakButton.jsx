dea7027bfd68282c6eb0238ade76de7166f8490c
Andrew <aawferris@gmail.com>
2020-10-15 10:06:31 -0400

    styled break button

--- productive.ly/src/Component_Library/BreakButton.jsx
+++ productive.ly/src/Component_Library/BreakButton.jsx
@@ -2,8 +2,8 @@ import React from 'react'
 
 function BreakButton() {
   return (
-    <div className='break-button'>
-      <a href="https://www.youtube.com/watch?v=s3Q80mk7bxE&ab_channel=IBeLovinTheJ5" target="_blank" rel="noopener noreferrer"><button>Take a Break!</button></a>
+    <div className='break-button' className='circleScaleBtn'>
+      <a href="https://www.youtube.com/watch?v=s3Q80mk7bxE&ab_channel=IBeLovinTheJ5" target="_blank" rel="noopener noreferrer"><button style={{border: 'none', background: 'transparent'}}>Take a Break!</button></a>
     </div>
   )
 }
