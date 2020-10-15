9ff9d371fc6313e9aee423918f9ede637e077fd8
Andrew <aawferris@gmail.com>
2020-10-15 15:18:39 -0400

    bif button

--- productive.ly/src/Component_Library/BreakButton.jsx
+++ productive.ly/src/Component_Library/BreakButton.jsx
@@ -2,8 +2,8 @@ import React from 'react'
 
 function BreakButton() {
   return (
-    <div className='break-button' id='circleScaleBtn'>
-      <a href="https://www.youtube.com/watch?v=s3Q80mk7bxE&ab_channel=IBeLovinTheJ5" target="_blank" rel="noopener noreferrer"><button style={{border: 'none', background: 'transparent'}}>Take a Break!</button></a>
+    <div className='break-button'>
+      <a href="https://www.youtube.com/watch?v=s3Q80mk7bxE&ab_channel=IBeLovinTheJ5" target="_blank" rel="noopener noreferrer"><button style={{border: 'none', borderRadius: '10px', background: 'white', padding: '7px'}}>Take a Break!</button></a>
     </div>
   )
 }
