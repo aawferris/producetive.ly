c50c6d9d42b0ba28133d7b4826e9314f7d7d830a
Andrew <aawferris@gmail.com>
2020-10-15 15:00:08 -0400

    nav bar styling added

--- productive.ly/src/Navbar.jsx
+++ productive.ly/src/Navbar.jsx
@@ -3,10 +3,29 @@ import { Link } from "react-router-dom";
 
 function Navbar() {
   return (
-    <header>
-      <nav id="navbar">
-        <Link to="/">Home</Link>
-        <Link to="/About">About Us</Link>
+    <header
+      id="navbar"
+      style={{
+        backdropFilter: 'brightness(30%)',
+        backgroundColor: 'transparent',
+      }}>
+      <nav>
+        <Link to="/"
+          style={{
+            color: 'white',
+            textDecoration: 'none',
+            padding: '5px',
+            margin: '5px'
+          }}
+        >Home</Link>
+        <Link to="/About"
+        style={{
+          color: 'white',
+          textDecoration: 'none',
+          padding: '5px',
+          margin: '5px'
+        }}
+        >About Us</Link>
       </nav>
     </header>
   )
